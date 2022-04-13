4c4857d815dda90eb3402502c72c99f1cfeae70f
kuosyuanwei <lavaday1225@gmail.com>
2022-04-13 17:17:13 +0800

    first commit

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "cool")
+}
+
+func InitialRouters() *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc("/", home)
+	return router
+}
+
+func Serve(port string, router *mux.Router) {
+	if err := http.ListenAndServe(":"+port, router); err != nil {
+		log.Fatalf("serve failed :%v", err)
+	}
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Route initialize
+	router := InitialRouters()
+	Serve("8080", router)
 }
